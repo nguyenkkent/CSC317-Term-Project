58849be394a74766b075a0a3471e00edf8e86ad6
Kent Nguyen <kentn@kents-air.gateway.sonic.net>
2022-10-27 14:49:07 -0700

    Added elements.

---
+++ application/public/js/Registration.js
@@ -0,0 +1,21 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+form.addEventListener("submit", e => {
+     e.preventDefault();
+     validInputs();
+});
+
+SpeechSynthesisErrorEvent = (element, message) => {
+
+};
+
+const validateInputs = () => {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+};
