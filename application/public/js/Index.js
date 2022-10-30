ecd0b400648e3da21dd2350241eb96446a1a6dbf
Kent Nguyen <kentn@kents-air.gateway.sonic.net>
2022-10-30 15:49:15 -0700

    Testing for fade out

--- application/public/js/Index.js
+++ application/public/js/Index.js
@@ -1,5 +1,3 @@
-
-
 /*
 <div class="product-card">
     <h4> class="prod-title" </h4>
@@ -8,11 +6,16 @@
 
 </div>    
 */
+
+let numberOfProducts = 50;
+
 function buildCards(container, card){
     let productCard = document.createElement("div");
     productCard.setAttribute("class", "product-card");
     productCard.addEventListener("click", function(ev){
         ev.currentTarget.remove();
+        numberOfProducts--;
+
     });
 
     let productTitle = document.createElement("h3");
@@ -43,53 +46,11 @@ function fetchProducts(){
             let container = document.getElementById("product-list");//root of the node tree
             data.forEach(function(card){
                 buildCards(container, card);
+                numberOfProducts++;
             });
         }); 
 };
 
 
-
-// function buildCardsUsingDOMApi(container, data){
-//     let cardDiv = document.createElement("div");
-//     cardDiv.setAttribute("class", "product-card");
-
-//     let imgElement = document.createElement("img");
-//     imgElement.setAttribute("class", "prod-img");
-//     imgElement.setAttribute("src", data.thumbnail);
-
-//     let prodInfoDiv = document.createElement("div");
-//     prodInfoDiv.setAttribute("class", "prod-info");
-
-//     let titleP = document.createElement("p");
-//     titleP.setAttribute("class", "product-title");
-//     titleP.appendChild(document.createTextNode(data.title));
-
-//     let costP = document.createElement("p");
-//     costP.setAttribute("class", "product-title");
-//     costP.appendChild(document.createTextNode(data.price));  
-    
-//     prodInfoDiv.appendChild(titleP);
-//     prodInfoDiv.appendChild(costP);
-
-//     cardDiv.append(imgElement);
-//     cardDiv.append(prodInfoDiv);
-
-//     container.appendChild(cardDiv);
-// }
-
-// function fetchProducts(){
-//     // fetch("https://jsonplaceholder.typicode.com/albums/2/photos")
-//     fetch("https://dummyjson.com/products")
-//         .then(function(response){
-//             return response.json();
-//         })
-//         .then(function(data){
-//             let products = data.products;
-//             let container = document.getElementById("product-list");
-//             products.forEach(function(product){
-//                  buildCardsUsingDOMApi(container, product)
-//             });
-//         }); 
-// };
-
 fetchProducts()
+console.log(numberOfProducts);
