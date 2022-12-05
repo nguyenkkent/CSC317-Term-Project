fe8895e200910a2b9003b619f38f290d5d7fa1e4
Kent Nguyen <kentn@sfs-wifi-dhcp-10-143-90-154.wifi.sfsu.edu>
2022-12-05 12:37:40 -0800

    Checkpoint - prior to /create fix.

--- application/routes/posts.js
+++ application/routes/posts.js
@@ -2,22 +2,21 @@ var express = require("express");
 var router = express.Router();
 const db = require('../conf/database');
 const multer = require("multer");
+const sharp = require("sharp");
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, '../uploads')
+      cb(null, 'public/images/uploads')
     },
-     
     filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.fieldname + '-' + uniqueSuffix)
+      let fileExt = file.mimetype.split("/"[1]);
+      cb(null,  `${file.fieldname}-${Date.now()}-${Math.round(Math.random() * 1E9)}.${fileExt}` )
     }
-  })
+  });
   
-  const upload = multer({ storage: storage })
+const upload = multer({ storage: storage })
   
-
-router.post("/create", function(req, res, next){
+router.post("/create", upload.single("uploadImage"), function(req, res, next){
     console.log(req);
     res.send();
 });
