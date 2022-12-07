dbc1b1f9cad8cc29e86f844fb5ca3e1bc77b1326
Kent Nguyen <kentn@sfs-wifi-dhcp-10-143-80-49.wifi.sfsu.edu>
2022-12-07 10:27:13 -0800

    Added search feature, moved search bar into navigation. Need to fix image display during search.

--- application/routes/posts.js
+++ application/routes/posts.js
@@ -4,38 +4,39 @@ const db = require('../conf/database');
 const multer = require("multer");
 const sharp = require("sharp");
 const { isLoggedIn } = require("../middleware/protectors");
+const { getPostsById, getRecentPosts } = require("../middleware/posts");
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, 'public/images/uploads')
+        cb(null, 'public/images/uploads')
     },
     filename: function (req, file, cb) {
-      let fileExt = file.mimetype.split("/")[1];  
-      cb(null,  `${file.fieldname}-${Date.now()}-${Math.round(Math.random() * 1E9)}.${fileExt}` )
+        let fileExt = file.mimetype.split("/")[1];
+        cb(null, `${file.fieldname}-${Date.now()}-${Math.round(Math.random() * 1E9)}.${fileExt}`)
     }
-  });
-  
+});
+
 const upload = multer({ storage: storage });
-  
-router.post("/create", isLoggedIn ,upload.single("uploadImage"), function(req, res, next){
+
+router.post("/create", isLoggedIn, upload.single("uploadImage"), function (req, res, next) {
     let uploadedFile = req.file.path;//assertion: file object exists
     let thumbnailName = `thumbnail-${req.file.filename}`
     let destinationOfThumbnail = `${req.file.destination}/${thumbnailName}`;
-    const {title, description } = req.body //.body contains key-value pairs of data submitted in the request body. title and description here are keys.
+    const { title, description } = req.body //.body contains key-value pairs of data submitted in the request body. title and description here are keys.
     const userId = req.session.userId;
 
     sharp(uploadedFile)
         .resize(200)
         .toFile(destinationOfThumbnail)
-        .then(function(){
+        .then(function () {
             let baseSQL = `INSERT INTO posts (title, description, image, thumbnail, fk_authorId) values (?,?,?,?,?)`;
-            return db.execute(baseSQL, [title, description, uploadedFile,destinationOfThumbnail, userId]);
+            return db.execute(baseSQL, [title, description, uploadedFile, destinationOfThumbnail, userId]);
             //remember that each promise needs to either error out or respond (res) then a return is needed;
         })
-        .then(function([results,fields]){
-            if (results && results.affectedRows==1){//if results exists and &7 results.affectedRows exists
+        .then(function ([results, fields]) {
+            if (results && results.affectedRows == 1) {//if results exists and &7 results.affectedRows exists
                 req.flash("success", "your post has been created!");
-                req.session.save(function(saveErr){
+                req.session.save(function (saveErr) {
                     res.redirect("/");//we have to save because the redict will happen much faster than the save will. save is async so we don't have to wait for that before redict
                 })
             }
@@ -43,11 +44,32 @@ router.post("/create", isLoggedIn ,upload.single("uploadImage"), function(req, r
         .catch(err => next(err));
 });
 
-router.get("/search", function(req, res, next){
-  // console.log(req.query);
-  // console.log(req);
-  let searchTerm = `%${req.query.searchTerm}%` //the % is a regex so %stuff can locate SOMEstuff
-  let originalSearchTerm = req.query.searchTerm;
-  res.render("index");
+router.get("/search", getRecentPosts, function (req, res, next) {
+    // console.log(req.query);
+    // console.log(req);
+    let searchTerm = `%${req.query.searchTerm}%` //the % is a regex so %stuff can locate SOMEstuff
+    let originalSearchTerm = req.query.searchTerm;
+    let baseSQL = `select id, title, description, thumbnail, concat_ws(" ", title, description) as haystack
+  from posts
+  having haystack like ?;`
+    //display columns id, title, description, thumnnail, contatentate title+description (display as haystack)
+    //from posts and haystack matches ?
+    db.execute(baseSQL, [searchTerm])
+        .then(function ([results, fields]) {
+            if (results.length == 0){
+                req.flash("error", "0 results found, displaying recent posts");
+                req.session.save(function(saveErr){
+                res.render("index");
+                })
+            }
+            else{
+                res.locals.results = results; //this creates a var results out of the results array from the db.execute
+                req.flash('success', `${results.length} results found`)
+                req.session.save(function(saveErr){
+                    res.render("index");
+                })                
+            }
+        })
+
 });
 module.exports = router;
