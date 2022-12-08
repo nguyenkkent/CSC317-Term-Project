259d0344dc9417a12b7182bd51eb212c94cb0a56
Kent Nguyen <kentn@sfs-wifi-dhcp-10-143-80-49.wifi.sfsu.edu>
2022-12-07 17:34:47 -0800

    Fixed minor issues up to getting values for comments.

--- application/public/js/viewpost.js
+++ application/public/js/viewpost.js
@@ -1,6 +1,23 @@
 function addNewComment(data){
 };
-document.getElementById("comment-button").addEventListener("click", function(ev){
-    console.log(ev);
-    console.log("inside event clicker");
-});
+// document.getElementById("comment-button").addEventListener("click", function(ev){
+//     console.log(ev);
+//     console.log("inside event clicker");
+// });
+
+document.getElementById("comment-button")
+    .addEventListener("click", function(event){
+        let commentTextElement = document.getElementById("comment-text");
+        let commentText = commentTextElement.value;
+        let postId = ev.currentTarget.dataset.postid;
+
+        fetch("/comments/create", {
+
+            method : "post",
+            headers : {
+                 "Content-Type" : "Application/json";
+            }
+
+        })//end of fetch
+
+    });
